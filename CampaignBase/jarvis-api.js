f2722c912461a05a5129882e4a7f18b41038d498
Nicolas Guadagno <nicolasguadagno@gmail.com>
2023-11-16 11:41:49 -0300

    hotfix(jarvis): Removed duplicated payouts

--- CampaignBase/jarvis-api.js
+++ CampaignBase/jarvis-api.js
@@ -99,13 +99,13 @@ function updateTSIJarvis() {
       t.eventsPayouts.forEach(function (variation) {
         //Check if the current variation _id is equal to the next. If so, just send the next one
         if (
+          payoutLength > 1 &&
           currentPayout < payoutLength - 1 &&
-          variation._id === t.eventsPayouts[currentPayout + 1]._id
+          variation._id === t.eventsPayouts[currentPayout - 1]._id
         ) {
           currentPayout++;
           return;
         }
-
 
         //Reset hours
         const startDate = new Date(variation.effectiveDate);
